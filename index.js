5d4c78d765e67a0b090ac6b498b8a5ddd2fcb7d9
developer.amit14jan@gmail.com <developer.amit14jan@gmail.com>
2021-11-28 15:32:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+app.listen(port , function(err){
+    if(err){console.log("Error "); return ;}
+    console.log("Server is running on the " , port);
+})
