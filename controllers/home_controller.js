3ddb702fa362192aecab7d3ade46bfeaf01cc9c8
developer.amit14jan@gmail.com <developer.amit14jan@gmail.com>
2021-11-29 16:44:26 +0530

    Add controller and router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+// creating the controller
+// syntax that we have to follow 
+// modules.exports.action_Name = function(request , response){}
+module.exports.home = function(request , response){
+    return response.end("The express js is running ");
+}
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+// use express router as middle ware
+app.use('/' , require('./routes/index'));
+
 app.listen(port , function(err){
     if(err){console.log("Error "); return ;}
     console.log("Server is running on the " , port);
